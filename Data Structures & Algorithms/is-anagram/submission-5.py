a731adf0af249e6c8a6b001ca98329fab29d85db
kaelpeterson14 <kaelpeterson03@outlook.com>
2026-09-11 13:35:18 -0600

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        #FreqMap of S
+        freqMap = {}
+        for c in s:
+            freqMap[c] = freqMap.get(c, 0)+1
+        for c in t:
+            freqMap[c] = freqMap.get(c, 0)-1
+            
+        for c in s:
+            if freqMap[c] != 0:
+                return False
+        return True
